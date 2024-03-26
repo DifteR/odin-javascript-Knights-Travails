d5b2fedf982431da376b0dcd8376024b0b5b564d
Tilen Marš <its0black0magic@gmail.com>
2024-03-26 11:54:27 +0100

    Create methonds for moving the knifgt

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
+class Knight {
+  constructor(position = [1, 0]) {
+    this.position = position;
+  }
+  checkIfOutOfBounds(newPosition) {
+    let notOutOfBounds = true;
+    if (
+      newPosition[0] < 0 ||
+      newPosition[0] > 7 ||
+      newPosition[1] < 0 ||
+      newPosition[1] > 7
+    ) {
+      notOutOfBounds = false;
+    }
+    return notOutOfBounds;
+  }
+  nextMove() {
+    let possibleNextMoves = new Array();
+  }
+  twoUpOneLeft() {
+    let current = this.position;
+    let x = current[0] - 1;
+    let y = current[1] + 2;
+    if (this.checkIfOutOfBounds([x], [y])) {
+      return current;
+    } else return -1;
+  }
+  twoUpOneRight() {
+    let current = this.position;
+    let x = current[0] + 1;
+    let y = current[1] + 2;
+    if (this.checkIfOutOfBounds([x], [y])) {
+      return current;
+    } else return -1;
+  }
+  twoDownOneRight() {
+    let current = this.position;
+    let x = current[0] + 1;
+    let y = current[1] - 2;
+    if (this.checkIfOutOfBounds([x], [y])) {
+      return current;
+    } else return -1;
+  }
+  twoDownOneLeft() {
+    let current = this.position;
+    let x = current[0] - 1;
+    let y = current[1] - 2;
+    if (this.checkIfOutOfBounds([x], [y])) {
+      return current;
+    } else return -1;
+  }
+}
 
+let newKnight = new Knight();
+console.log(newKnight.twoUpOneLeft());
