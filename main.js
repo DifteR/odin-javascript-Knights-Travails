0558ab42820e6b3437d18d1ffeca713f36410256
Tilen Marš <its0black0magic@gmail.com>
2024-04-06 07:42:43 +0000

    Working solution

--- main.js
+++ main.js
@@ -1,55 +1,119 @@
 class Knight {
-  constructor(position = [1, 0]) {
+  constructor(position = [5,0], goalPosition = [5,0]) {
     this.position = position;
+    this.goalPosition = goalPosition;
+    this.possibleSolutionsArray = new Array;
+    this.possibleSolutionsArray[0] = this.position;
+    this.numberOfTurns = 0;
   }
+
   checkIfOutOfBounds(newPosition) {
     let notOutOfBounds = true;
+    
     if (
       newPosition[0] < 0 ||
       newPosition[0] > 7 ||
       newPosition[1] < 0 ||
       newPosition[1] > 7
-    ) {
-      notOutOfBounds = false;
+      ) {
+        notOutOfBounds = false;
+      }
+      return notOutOfBounds;
     }
-    return notOutOfBounds;
+    nextMove() {
+      let currentPos = new Array;
+      
+      while (true) {
+        if (this.possibleSolutionsArray.some(pos => JSON.stringify(pos) === JSON.stringify(this.goalPosition))) {
+          console.log(this.numberOfTurns);
+          return;
+        }
+        else{
+          //currentPos = [...this.possibleSolutionsArray.shift()];
+          this.possibleSolutionsArray.forEach(el => {
+            this.calculateAllPossible(el);
+          });
+          this.numberOfTurns++;
+          console.log(this.possibleSolutionsArray);
+        }
+      }
+    }
+    calculateAllPossible(pos){
+    this.twoUpOneLeft(pos);
+    this.oneUpTwoLeft(pos);
+    this.twoDownOneRight(pos);
+    this.oneUpTwoRight(pos);
+    this.twoDownOneLeft(pos);
+    this.oneDownTwoLeft(pos);
+    this.twoDownOneRight(pos);
+    this.oneDownTwoRight(pos);
   }
-  nextMove() {
-    let possibleNextMoves = new Array();
+  twoUpOneLeft(pos) {
+    let current = [...pos];
+    current[0] -=  1;
+    current[1] += 2;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    }
+    return 0;
   }
-  twoUpOneLeft() {
-    let current = this.position;
-    let x = current[0] - 1;
-    let y = current[1] + 2;
-    if (this.checkIfOutOfBounds([x], [y])) {
-      return current;
-    } else return -1;
+  oneUpTwoLeft(pos) {
+    let current = [...pos];
+    current[0] -=  2;
+    current[1] += 1;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
   }
-  twoUpOneRight() {
-    let current = this.position;
-    let x = current[0] + 1;
-    let y = current[1] + 2;
-    if (this.checkIfOutOfBounds([x], [y])) {
-      return current;
-    } else return -1;
+  twoUpOneRight(pos) {
+    let current = [...pos];
+    current[0] +=  1;
+    current[1] += 2;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
   }
-  twoDownOneRight() {
-    let current = this.position;
-    let x = current[0] + 1;
-    let y = current[1] - 2;
-    if (this.checkIfOutOfBounds([x], [y])) {
-      return current;
-    } else return -1;
+  oneUpTwoRight(pos) {
+    let current = [...pos];
+    current[0] +=  2;
+    current[1] += 1;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
   }
-  twoDownOneLeft() {
-    let current = this.position;
-    let x = current[0] - 1;
-    let y = current[1] - 2;
-    if (this.checkIfOutOfBounds([x], [y])) {
-      return current;
-    } else return -1;
+  twoDownOneRight(pos) {
+    let current = [...pos];
+    current[0] +=  1;
+    current[1] -= 2;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
+  }
+  oneDownTwoRight(pos) {
+    let current = [...pos];
+    current[0] +=  2;
+    current[1] -= 1;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
+  }
+  twoDownOneLeft(pos) {
+    let current = [...pos];
+    current[0] -=  1;
+    current[1] -= 2;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
+  }
+  oneDownTwoLeft(pos) {
+    let current = [...pos];
+    current[0] -=  2;
+    current[1] -= 1;
+    if (this.checkIfOutOfBounds(current)) {
+      this.possibleSolutionsArray.push(current);
+    } else return;
   }
 }
 
-let newKnight = new Knight();
-console.log(newKnight.twoUpOneLeft());
+let startPos1 = new Knight([5,5], [5,0]);
+startPos1.nextMove();
